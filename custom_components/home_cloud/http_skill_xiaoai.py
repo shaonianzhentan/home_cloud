87c959a4ebb21ff4f2683ee63bf8eca4676c7ca9
shaonianzhentan <ilovevar@gmail.com>
2023-12-11 23:51:14 +0800

    优化小爱技能

--- custom_components/home_cloud/http_skill_xiaoai.py
+++ custom_components/home_cloud/http_skill_xiaoai.py
@@ -2,8 +2,7 @@ import json
 import logging
 from homeassistant.components.http import HomeAssistantView
 
-from .const import API_SKILL_XIAOAI
-from .utils import matcher_query_state, find_entity
+from .const import API_SKILL_XIAOAI, CONVERSATION_ASSISTANT
 
 from .api_xiaoai import (XiaoAIAudioItem, XiaoAIDirective, XiaoAIOpenResponse,
                          XiaoAIResponse, XiaoAIStream, XiaoAIToSpeak, XiaoAITTSItem,
@@ -12,7 +11,6 @@ from .api_xiaoai import (XiaoAIAudioItem, XiaoAIDirective, XiaoAIOpenResponse,
 from .manifest import manifest
 
 DOMAIN = manifest.domain
-_LOGGER = logging.getLogger(__name__)
 
 # 文本回复
 def build_text_message(to_speak, is_session_end, open_mic, not_understand=False):
@@ -25,10 +23,8 @@ def build_text_message(to_speak, is_session_end, open_mic, not_understand=False)
                                                   response=xiao_ai_response))
     return response
 
-# 音乐回复
-
-
 def build_music_message(to_speak, mp3_urls):
+    ''' 音乐回复 '''
     all_list = []
     if to_speak is not None:
         info_tts = XiaoAIDirective(
@@ -52,15 +48,14 @@ def build_music_message(to_speak, mp3_urls):
 # 格式转换
 
 
-async def parse_input(event, hass):
+async def parse_input(event, hass, api_cloud):
     req = xiaoai_request(event)
     text = req.query
-    # 判断当前用户是否是自己
-    options = hass.data[DOMAIN]
-    open_mic = options.get('open_mic', True)
-    user_id = options.get('open_id', '')
-    if user_id != '' and user_id != req.session.user.user_id:
-        return build_text_message('我真的好笨笨哦，不知道你在说啥，换个方式叫我吧', is_session_end=True, open_mic=False, not_understand=True)
+    open_mic = True
+
+    if req.session.user.access_token != api_cloud._key:
+        return build_text_message('抱歉，您没有权限', is_session_end=True, open_mic=False, not_understand=True)
+    
     # 插槽：req.request.slot_info.intent_name
     intent_name = ''
     if hasattr(req.request.slot_info, 'intent_name'):
@@ -81,31 +76,22 @@ async def parse_input(event, hass):
 
     return build_text_message('我没听懂欸', is_session_end=True, open_mic=False)
 
-# 消息处理
-
 
 async def conversation_process(hass, text, open_mic):
+    ''' 消息处理 '''
     is_session_end = (open_mic == False)
-    hass.async_create_task(hass.services.async_call(
-        'conversation', 'process', {'text': text}))
-    # 如果配置到了查询，则不进入系统意图
-    result = matcher_query_state(text)
-    if result is not None:
-        friendly_name = result
-        state = await find_entity(hass, friendly_name)
-        if state is not None:
-            message = f'{friendly_name}的状态是{state.state}'
-            if open_mic:
-                message += '，请问还有什么事吗？'
-            return build_text_message(message, is_session_end, open_mic)
 
-    message = '收到'
+    message = '请安装最新版语音助手插件'
+    conversation = hass.data.get(CONVERSATION_ASSISTANT)
+    if conversation is not None:
+        res = await conversation.recognize(text)
+        intent_result = res.response
+        message = intent_result.speech['plain']['speech']
+
     if open_mic:
         message += '，还有什么事吗？'
+
     return build_text_message(message, is_session_end, open_mic)
-
-# 网关视图
-
 
 class HttpSkillXiaoai(HomeAssistantView):
 
@@ -116,17 +102,18 @@ class HttpSkillXiaoai(HomeAssistantView):
     async def post(self, request):
         data = await request.json()
         hass = request.app["hass"]
-        options = hass.data[DOMAIN]
 
-        if options.get('debug', False) == True:
+        api_cloud = hass.data[manifest.domain]
+
+        if api_cloud._debug:
             await hass.services.async_call('persistent_notification', 'create', {
                 'title': '接收信息',
                 'message': json.dumps(data, indent=2)
             })
 
-        response = await parse_input(data, hass)
+        response = await parse_input(data, hass, api_cloud)
 
-        if options.get('debug', False) == True:
+        if api_cloud._debug:
             await hass.services.async_call('persistent_notification', 'create', {
                 'title': '发送信息',
                 'message': json.dumps(response, indent=2)
